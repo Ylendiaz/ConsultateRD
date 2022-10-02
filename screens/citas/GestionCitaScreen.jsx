44ee2023be7c82546f7b5dc70f61d201fc29ffaf
Jonathan Diaz <jonaalbert.adm@gmail.com>
2022-10-02 18:47:40 -0400

    Texto test

--- screens/citas/GestionCitaScreen.jsx
+++ screens/citas/GestionCitaScreen.jsx
@@ -6,10 +6,11 @@ import AppNavigator from '../../navigator/Navigator';
 const GestionCitaScreen = ({navigation}) => {
     return<>
     <View style={{height:100, width:100}}>
-        <Text>Pantalla de gestion de citas</Text>
+        <Text>Pantalla de gestion de citas medicas</Text>
     </View>
     </>
     
 }
+
 
 export default GestionCitaScreen;
