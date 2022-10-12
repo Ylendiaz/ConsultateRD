370a469b623850d032b7460b1b4882f313daa1d3
computermaster44 <59265263+computermaster44@users.noreply.github.com>
2022-10-12 00:39:00 -0400

    Update Especialidades.js

--- API/Especialidades.js
+++ API/Especialidades.js
@@ -3,5 +3,12 @@ export default [
         especialidadId: 1,
         nombreEspecialidad: "Médico General",
         especialidadesDoctor: []
+    },
+    
+    {
+        especialidadId: 2,
+        nombreEspecialidad: "Endocrinologo",
+        especialidadesDoctor: []
+   
     }
 ]
