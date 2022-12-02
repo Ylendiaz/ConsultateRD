8976f6eb8b5ff14ab6c857a6071f613dccfaa128
Ylen Diaz <ylendiazpolanco@gmail.com>
2022-12-01 23:07:27 -0400

    validar por id del doctor. componente para homeScreen y GestionCita

--- components/CitasAgendadas.jsx
+++ components/CitasAgendadas.jsx
@@ -3,11 +3,73 @@ import {SafeAreaView, ScrollView, StyleSheet, View, Text, FlatList, TouchableOpa
 import CalendarPickerModal from 'react-native-calendar-picker';
 //import { SafeAreaView } from "react-native-safe-area-context";
 import { withSafeAreaInsets } from 'react-native-safe-area-context';
+import Paciente from '../API/Paciente';
+
 
 
 const CitasAgendadas = (props) => {
 
-    const { citas } = props;
+    const { citas, login1 } = props;
+    const [apidataPaciente, apisetDataPaciente] = useState([]);
+    const login = [{ loginId: 7, rol: true }];
+    
+    //const findRol = () => {};
+    // ----------------Consumir API tabla Usuario Pacientes-----------------
+    useEffect(() => {
+        fetchData('https://consultaterd.azurewebsites.net/api/UsuarioPacientes');
+    }, [])
+    
+
+    const fetchData = async (url) => {
+        try {
+            const response = await fetch(url);
+            const json = await response.json();
+            apisetDataPaciente(json);
+            // console.log(json);
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+    const [apidataCentros, apisetDataCentros] = useState([]);
+    
+    // ----------------Consumir API tabla Centro Medico-----------------
+    useEffect(() => {
+        fetchDataCentros('https://consultaterd.azurewebsites.net/api/CentroMedico');
+    }, [])
+
+    const fetchDataCentros = async (url) => {
+        try {
+            const response = await fetch(url);
+            const json = await response.json();
+            const newarray = json.map((item) => {
+                return { key: item.centroMedicoId, value: item.centroMedicoNombre }
+            })
+            apisetDataCentros(newarray);
+
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    // ----------------Consumir API tabla Usuario Doctores-----------------
+    const [apidataDoctores, apisetDoctores] = useState([]);
+    
+    useEffect(() => {
+        fetchDataDoctores('https://consultaterd.azurewebsites.net/api/UsuarioDoctores');
+    }, [])
+
+    const fetchDataDoctores = async (url) => {
+        try {
+            const response = await fetch(url);
+            const json = await response.json();
+            apisetDoctores(json);
+        
+        } catch (error) {
+            console.error(error);
+        }
+    };
   
     return (
   
@@ -20,7 +82,17 @@ const CitasAgendadas = (props) => {
                         <View style={styles.listViewContent}>
                             
                             <View style={styles.listTextView}>
-                                <Text style={{ color: "black", marginBottom: 8 }}>{item.citaFecha} {item.pacienteId} {item.citasHoraInicio}</Text>
+                                <Text style={{ color: "black", marginBottom: 8 }}>{item.citaFecha}              {item.citasHoraInicio}</Text>
+                                
+                                {login.filter(x => x.loginId ==  item.loginId).map(x => {return x.rol})== true
+                                ?    <Text>{apidataDoctores.filter(x => x.doctorId == item.doctorId).map(y => {return y.nombreDoctor + " " + y.apellidoDoctor})}</Text>
+                                    : <Text>{apidataPaciente.filter(x => x.pacienteId == item.pacienteId).map(y => {return y.nombrePaciente + " " + y.apellidoPaciente})}</Text>}
+                                    
+                                                                       
+                                    <Text>{apidataCentros.filter(x => x.key == item.centroMedicoId).map(y => {return y.value})}</Text>
+
+
+                                
                                 
                                 
                             </View>
